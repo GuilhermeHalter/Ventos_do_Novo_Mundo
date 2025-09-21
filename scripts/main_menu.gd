1876e01d0afef1b33b8df1c0428e06776cdf7642
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2025-09-21 11:42:03 -0300

    Atualizando projeto: Corrigindo Bugs

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -18,6 +18,10 @@ func _on_btn_jogar_pressed() -> void:
 	tween.tween_property(fade, "modulate:a", 1.0, 1.0)
 	tween.finished.connect(_start_cutscene)
 
+func _on_btn_sair_pressed() -> void:
+	get_tree().quit()
+	
+
 func _start_cutscene():
 	# Troca de cena diretamente para a cutscene
 	get_tree().change_scene_to_file("res://scenes/Cutscene.tscn")
